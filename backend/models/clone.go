dd1803d5c3408cbe9ad317eff862329a75c9af5b
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-02-02 22:10:49 -0800

    Add clone method on Stream and by extension some children of Stream
    - By cloning this data, we can avoid data races when multiple goroutines
      are trying to read the streams
    - Properly handles pathologic cases like when pointer entries are nil
      or when container types are empty

---
+++ backend/models/clone.go
@@ -0,0 +1,117 @@
+package models
+
+// Clone returns a deep copy of the Stream struct. Any changes made to this copy
+// should not affect the original struct.
+func (s Stream) Clone() Stream {
+	s.Place = s.Place.Clone()
+	s.Enmity = s.Enmity.Clone()
+
+	if s.CraftingInfo != nil {
+		craftInfoClone := *s.CraftingInfo
+		s.CraftingInfo = &craftInfoClone
+	}
+
+	if len(s.EntitiesMap) > 0 {
+		entitiesMap := make(map[uint64]*Entity)
+		for id, ent := range s.EntitiesMap {
+			if ent == nil {
+				entitiesMap[id] = nil
+				continue
+			}
+			entClone := ent.Clone()
+			entitiesMap[id] = &entClone
+		}
+		s.EntitiesMap = entitiesMap
+	}
+
+	return s
+}
+
+// Clone returns a deep copy of the Place struct. Any changes made to this copy
+// should not affect the original struct.
+func (p Place) Clone() Place {
+	if len(p.Maps) > 0 {
+		maps := make([]MapInfo, len(p.Maps))
+		copy(maps, p.Maps)
+		p.Maps = maps
+	}
+	return p
+}
+
+// Clone returns a deep copy of the Enmity struct. Any changes made to this copy
+// should not affect the original struct.
+func (e Enmity) Clone() Enmity {
+	if len(e.TargetHateRanking) > 0 {
+		hateRankings := make([]HateRanking, len(e.TargetHateRanking))
+		copy(hateRankings, e.TargetHateRanking)
+		e.TargetHateRanking = hateRankings
+	}
+
+	if len(e.NearbyEnemyHate) > 0 {
+		enemyHate := make([]HateEntry, len(e.NearbyEnemyHate))
+		copy(enemyHate, e.NearbyEnemyHate)
+		e.NearbyEnemyHate = enemyHate
+	}
+	return e
+}
+
+// Clone returns a deep copy of the Entity struct. Any changes made to this copy
+// should not affect the original struct.
+func (e Entity) Clone() Entity {
+	if e.BNPCInfo != nil {
+		bNPCInfoClone := e.BNPCInfo.Clone()
+		e.BNPCInfo = &bNPCInfoClone
+	}
+
+	if e.LastAction != nil {
+		lastActionClone := e.LastAction.Clone()
+		e.LastAction = &lastActionClone
+	}
+
+	if e.LastAction != nil {
+		castingInfoClone := *e.CastingInfo
+		e.CastingInfo = &castingInfoClone
+	}
+
+	if len(e.Statuses) > 0 {
+		statuses := make([]*Status, len(e.Statuses))
+		for i, v := range e.Statuses {
+			if v == nil {
+				continue
+			}
+			vClone := *v
+			statuses[i] = &vClone
+		}
+		e.Statuses = statuses
+
+	}
+
+	return e
+}
+
+// Clone returns a deep copy of the NPCInfo struct. Any changes made to this copy
+// should not affect the original struct.
+func (n NPCInfo) Clone() NPCInfo {
+	if n.Name != nil {
+		nameClone := *n.Name
+		n.Name = &nameClone
+	}
+
+	if n.Size != nil {
+		sizeClone := *n.Size
+		n.Size = &sizeClone
+	}
+
+	return n
+}
+
+// Clone returns a deep copy of the Action struct. Any changes made to this copy
+// should not affect the original struct.
+func (a Action) Clone() Action {
+	if len(a.Effects) > 0 {
+		effects := make([]ActionEffect, len(a.Effects))
+		copy(effects, a.Effects)
+		a.Effects = effects
+	}
+	return a
+}
---
+++ backend/models/clone_test.go
@@ -0,0 +1,145 @@
+package models_test
+
+import (
+	"github.com/ff14wed/sibyl/backend/models"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Stream Clone", func() {
+	var stream *models.Stream
+
+	BeforeEach(func() {
+		bNPCInfoName := "Bar"
+		bNPCInfoSize := 1.5
+
+		stream = &models.Stream{
+			PID:         1234,
+			CharacterID: 4567,
+			Place: models.Place{
+				MapID: 20,
+				Maps: []models.MapInfo{
+					models.MapInfo{ID: "some-map"},
+				},
+			},
+			Enmity: models.Enmity{
+				TargetHateRanking: []models.HateRanking{
+					models.HateRanking{ActorID: 2345, Hate: 1000},
+				},
+				NearbyEnemyHate: []models.HateEntry{
+					models.HateEntry{EnemyID: 3456, HatePercent: 99},
+				},
+			},
+
+			CraftingInfo: &models.CraftingInfo{
+				RecipeID: 900,
+			},
+			EntitiesMap: map[uint64]*models.Entity{
+				1: &models.Entity{
+					ID: 1, Index: 2, Name: "FooBar",
+					BNPCInfo: &models.NPCInfo{
+						Name: &bNPCInfoName,
+						Size: &bNPCInfoSize,
+					},
+					LastAction: &models.Action{
+						TargetID: 5678,
+						Effects: []models.ActionEffect{
+							models.ActionEffect{TargetID: 2345},
+						},
+					},
+					CastingInfo: &models.CastingInfo{ActionID: 100},
+					Statuses: []*models.Status{
+						&models.Status{ID: 50},
+					},
+				},
+			},
+		}
+	})
+
+	It("produces an identical copy of the original struct", func() {
+		streamClone := stream.Clone()
+		Expect(streamClone).To(Equal(*stream))
+	})
+
+	DescribeTable("changes on the clone of the stream should not affect the original copy",
+		func(modifier func(*models.Stream)) {
+			streamClone := stream.Clone()
+			modifier(&streamClone)
+			Expect(streamClone).ToNot(Equal(*stream))
+		},
+		Entry("Stream", func(s *models.Stream) {
+			s.CharacterID = 5678
+		}),
+		Entry("stream.Place.Maps", func(s *models.Stream) {
+			s.Place.Maps[0].ID = "foo-map"
+		}),
+		Entry("stream.Enmity.TargetHateRanking", func(s *models.Stream) {
+			s.Enmity.TargetHateRanking[0].Hate = 1001
+		}),
+		Entry("stream.Enmity.NearbyEnemyHate", func(s *models.Stream) {
+			s.Enmity.NearbyEnemyHate[0].HatePercent = 100
+		}),
+		Entry("stream.CraftingInfo", func(s *models.Stream) {
+			s.CraftingInfo.RecipeID = 200
+		}),
+		Entry("stream.EntitiesMap", func(s *models.Stream) {
+			s.EntitiesMap[2] = &models.Entity{ID: 2, Name: "Baah", Index: 1}
+		}),
+		Entry("Entity", func(s *models.Stream) {
+			s.EntitiesMap[1].Name = "BarFoo"
+		}),
+		Entry("entity.BNPCInfo", func(s *models.Stream) {
+			s.EntitiesMap[1].BNPCInfo.NameID = 1
+		}),
+		Entry("entity.BNPCInfo.Name", func(s *models.Stream) {
+			newName := "Baz"
+			s.EntitiesMap[1].BNPCInfo.Name = &newName
+		}),
+		Entry("entity.BNPCInfo.Size", func(s *models.Stream) {
+			newSize := 2.0
+			s.EntitiesMap[1].BNPCInfo.Size = &newSize
+		}),
+		Entry("entity.LastAction", func(s *models.Stream) {
+			s.EntitiesMap[1].LastAction.TargetID = 6789
+		}),
+		Entry("entity.LastAction.Effects", func(s *models.Stream) {
+			s.EntitiesMap[1].LastAction.Effects[0].TargetID = 3456
+		}),
+		Entry("entity.CastingInfo", func(s *models.Stream) {
+			s.EntitiesMap[1].CastingInfo.ActionID = 101
+		}),
+		Entry("entity.Statuses", func(s *models.Stream) {
+			s.EntitiesMap[1].Statuses[0].ID = 51
+		}),
+	)
+
+	Describe("with a minimal stream", func() {
+		BeforeEach(func() {
+			stream = &models.Stream{
+				PID: 1234,
+				EntitiesMap: map[uint64]*models.Entity{
+					1: &models.Entity{ID: 1, Index: 2, Name: "FooBar"},
+					2: &models.Entity{ID: 1, Index: 2, Name: "FooBar", BNPCInfo: &models.NPCInfo{}},
+					3: nil,
+				},
+			}
+		})
+
+		It("produces an identical copy of the original struct", func() {
+			streamClone := stream.Clone()
+			Expect(streamClone).To(Equal(*stream))
+		})
+	})
+
+	Describe("with an empty stream", func() {
+		BeforeEach(func() {
+			stream = &models.Stream{PID: 1234}
+		})
+
+		It("produces an identical copy of the original struct", func() {
+			streamClone := stream.Clone()
+			Expect(streamClone).To(Equal(*stream))
+		})
+	})
+})
