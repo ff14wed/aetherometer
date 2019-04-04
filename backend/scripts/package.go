0d785e5ac4e619d4186c49b423a5e0fbb3ad4d3b
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-04-04 10:17:36 -0400

    Use build tag with tools imports to prevent annoying harmless errors

--- backend/scripts/package.go
+++ backend/scripts/package.go
@@ -1,9 +1,1 @@
 package scripts
-
-// Import packages necessary for tools but not necessary for
-// compilation
-import (
-	_ "github.com/99designs/gqlgen/cmd"
-	_ "github.com/99designs/gqlgen/codegen"
-	_ "github.com/maxbrunsfeld/counterfeiter/v6"
-)
