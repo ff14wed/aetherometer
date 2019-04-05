a797d652a129623e6c8d53e5244e8513a3cd73a4
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-04-05 10:29:58 -0400

    Add stream manager responsible for managing handlers for streams

---
+++ backend/stream/manager.go
@@ -0,0 +1,127 @@
+package stream
+
+import (
+	"fmt"
+
+	"github.com/ff14wed/sibyl/backend/store"
+	"github.com/ff14wed/sibyl/backend/store/update"
+	"github.com/ff14wed/xivnet/v3"
+	"github.com/thejerf/suture"
+	"go.uber.org/zap"
+)
+
+type HandlerFactoryArgs struct {
+	PID         int
+	IngressChan <-chan *xivnet.Frame
+	EgressChan  <-chan *xivnet.Frame
+	UpdateChan  chan<- store.Update
+	Generator   update.Generator
+	Logger      *zap.Logger
+}
+
+// HandlerFactory defines a factory for creating a handler capable of processing
+// updates from the stream provider
+type HandlerFactory func(h HandlerFactoryArgs) suture.Service
+
+// Manager is a process responsible for watching stream created or stream
+// closed events from all adapters
+type Manager struct {
+	generator        update.Generator
+	updateChan       chan<- store.Update
+	streamSupervisor *suture.Supervisor
+	handlerFactory   HandlerFactory
+	logger           *zap.Logger
+
+	stop         chan struct{}
+	streamTokens map[int]suture.ServiceToken
+	providers    map[int]Provider
+
+	streamUp   chan Provider
+	streamDown chan int
+}
+
+// NewManager returns a new stream Manager.
+func NewManager(
+	generator update.Generator,
+	updateChan chan<- store.Update,
+	streamSupervisor *suture.Supervisor,
+	handlerFactory HandlerFactory,
+	logger *zap.Logger,
+) *Manager {
+	return &Manager{
+		generator:        generator,
+		updateChan:       updateChan,
+		streamSupervisor: streamSupervisor,
+		handlerFactory:   handlerFactory,
+		logger:           logger.Named("stream-manager"),
+
+		stop:         make(chan struct{}),
+		streamTokens: make(map[int]suture.ServiceToken),
+		providers:    make(map[int]Provider),
+
+		streamUp:   make(chan Provider, 64),
+		streamDown: make(chan int, 64),
+	}
+}
+
+// Serve runs the stream manager. It is responsible for spinning up stream
+// handlers whenever new streams are created and shutting down stream handlers
+// when streams are closed.
+func (m *Manager) Serve() {
+	m.logger.Info("Running")
+	for {
+		select {
+		case sp := <-m.streamUp:
+			pid := sp.PID()
+			ingressChan := sp.SubscribeIngress()
+			egressChan := sp.SubscribeEgress()
+			sh := m.handlerFactory(HandlerFactoryArgs{
+				PID:         pid,
+				IngressChan: ingressChan,
+				EgressChan:  egressChan,
+				UpdateChan:  m.updateChan,
+				Generator:   m.generator,
+				Logger:      m.logger,
+			})
+			token := m.streamSupervisor.Add(sh)
+			m.streamTokens[pid] = token
+			m.providers[pid] = sp
+		case pid := <-m.streamDown:
+			err := m.streamSupervisor.Remove(m.streamTokens[pid])
+			if err != nil {
+				m.logger.Error("Error removing stream", zap.Int("streamID", pid), zap.Error(err))
+			}
+			delete(m.streamTokens, pid)
+			delete(m.providers, pid)
+		case <-m.stop:
+			m.logger.Info("Stopping...")
+			return
+		}
+	}
+}
+
+// Stop will shutdown this service eventually, but will not wait on it to stop
+func (m *Manager) Stop() {
+	close(m.stop)
+}
+
+// SendRequest forwards a request for a given stream ID to the correct stream
+// Provider.
+func (m *Manager) SendRequest(pid int, req []byte) ([]byte, error) {
+	if provider, found := m.providers[pid]; found {
+		return provider.SendRequest(req)
+	}
+	return nil, fmt.Errorf("stream provider %d not found", pid)
+}
+
+// StreamUp returns a channel that allows an upstream service to notify the
+// manager that a new stream has been created.
+func (m *Manager) StreamUp() chan<- Provider {
+	return m.streamUp
+}
+
+// StreamDown returns a channel that allows an upstream service to notify the
+// manager that a stream has closed.
+func (m *Manager) StreamDown() chan<- int {
+	return m.streamDown
+}
---
+++ backend/stream/manager_test.go
@@ -0,0 +1,190 @@
+package stream_test
+
+import (
+	"net/url"
+	"sync"
+	"sync/atomic"
+
+	"github.com/ff14wed/sibyl/backend/store"
+	"github.com/ff14wed/sibyl/backend/store/update"
+	"github.com/ff14wed/sibyl/backend/stream"
+	"github.com/ff14wed/sibyl/backend/stream/streamfakes"
+	"github.com/ff14wed/sibyl/backend/testhelpers"
+	"github.com/ff14wed/xivnet/v3"
+	"github.com/thejerf/suture"
+	"go.uber.org/zap"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+)
+
+type FakeHandler struct {
+	serveCalled uint32
+	stopCalled  uint32
+	stop        chan struct{}
+}
+
+func (f *FakeHandler) Serve() {
+	atomic.StoreUint32(&f.serveCalled, 1)
+	<-f.stop
+}
+
+func (f *FakeHandler) ServeCalled() bool {
+	return atomic.LoadUint32(&f.serveCalled) == 1
+}
+
+func (f *FakeHandler) Stop() {
+	atomic.StoreUint32(&f.stopCalled, 1)
+	close(f.stop)
+}
+
+func (f *FakeHandler) StopCalled() bool {
+	return atomic.LoadUint32(&f.stopCalled) == 1
+}
+
+var _ = Describe("Manager", func() {
+	var (
+		manager    *stream.Manager
+		supervisor *suture.Supervisor
+
+		generator        update.Generator
+		updateChan       chan<- store.Update
+		streamSupervisor *suture.Supervisor
+
+		handlerFactoryArgs stream.HandlerFactoryArgs
+		fakeHandler        *FakeHandler
+
+		logBuf *testhelpers.LogBuffer
+		logger *zap.Logger
+
+		once sync.Once
+	)
+
+	BeforeEach(func() {
+		var err error
+		once.Do(func() {
+			logBuf = new(testhelpers.LogBuffer)
+			err = zap.RegisterSink("managertest", func(*url.URL) (zap.Sink, error) {
+				return logBuf, nil
+			})
+		})
+		logBuf.Reset()
+		Expect(err).ToNot(HaveOccurred())
+		zapCfg := zap.NewDevelopmentConfig()
+		zapCfg.OutputPaths = []string{"managertest://"}
+		logger, err = zapCfg.Build()
+		Expect(err).ToNot(HaveOccurred())
+
+		generator = update.NewGenerator(nil)
+		updateChan = make(chan store.Update)
+		fakeHandler = &FakeHandler{stop: make(chan struct{})}
+		handlerFactory := func(args stream.HandlerFactoryArgs) suture.Service {
+			handlerFactoryArgs = args
+			return fakeHandler
+		}
+		streamSupervisor = suture.New("stream-supervisor", suture.Spec{
+			Log: func(line string) {
+				logger.Named("stream-supervisor").Info(line)
+			},
+		})
+
+		manager = stream.NewManager(
+			generator,
+			updateChan,
+			streamSupervisor,
+			handlerFactory,
+			logger,
+		)
+
+		supervisor = suture.New("test-manager", suture.Spec{
+			Log: func(line string) {
+				_, _ = GinkgoWriter.Write([]byte(line))
+			},
+			FailureThreshold: 1,
+		})
+		supervisor.ServeBackground()
+		_ = supervisor.Add(streamSupervisor)
+		_ = supervisor.Add(manager)
+	})
+
+	AfterEach(func() {
+		supervisor.Stop()
+	})
+
+	It(`logs "Running" on startup`, func() {
+		Eventually(logBuf).Should(gbytes.Say("stream-manager.*Running"))
+	})
+
+	It(`logs "Stopping..." on shutdown`, func() {
+		supervisor.Stop()
+		Eventually(logBuf).Should(gbytes.Say("stream-manager.*Stopping..."))
+	})
+
+	It("logs an error when attempting to remove a non-existent stream", func() {
+		manager.StreamDown() <- 1234
+		Eventually(logBuf).Should(gbytes.Say("Error removing stream.*1234"))
+	})
+
+	Context("when a new stream is created", func() {
+		var (
+			fakeProvider *streamfakes.FakeProvider
+			ingressChan  <-chan *xivnet.Frame
+			egressChan   <-chan *xivnet.Frame
+		)
+
+		BeforeEach(func() {
+			fakeProvider = new(streamfakes.FakeProvider)
+			fakeProvider.PIDReturns(1234)
+			ingressChan = make(chan *xivnet.Frame)
+			egressChan = make(chan *xivnet.Frame)
+			fakeProvider.SubscribeIngressReturns(ingressChan)
+			fakeProvider.SubscribeEgressReturns(egressChan)
+			fakeProvider.SendRequestReturns([]byte("ack"), nil)
+			manager.StreamUp() <- fakeProvider
+		})
+
+		It("creates a new Handler for the stream", func() {
+			Eventually(fakeHandler.ServeCalled).Should(BeTrue())
+			Expect(handlerFactoryArgs.PID).To(Equal(1234))
+			Expect(handlerFactoryArgs.IngressChan).To(Equal(ingressChan))
+			Expect(handlerFactoryArgs.EgressChan).To(Equal(egressChan))
+			Expect(handlerFactoryArgs.UpdateChan).To(Equal(updateChan))
+			Expect(handlerFactoryArgs.Generator).To(Equal(generator))
+		})
+
+		Describe("SendRequest", func() {
+			BeforeEach(func() {
+				Eventually(fakeHandler.ServeCalled).Should(BeTrue())
+			})
+			It("handles requests for the stream", func() {
+				resp, err := manager.SendRequest(1234, []byte("foo"))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(resp).To(Equal([]byte("ack")))
+			})
+
+			It("errors when the stream doesn't exist", func() {
+				_, err := manager.SendRequest(5678, nil)
+				Expect(err).To(MatchError("stream provider 5678 not found"))
+			})
+		})
+
+		Context("when the stream is closed", func() {
+			BeforeEach(func() {
+				Eventually(fakeHandler.ServeCalled).Should(BeTrue())
+				Consistently(fakeHandler.StopCalled).Should(BeFalse())
+				manager.StreamDown() <- 1234
+			})
+
+			It("stops the Handler for the stream", func() {
+				Eventually(fakeHandler.StopCalled).Should(BeTrue())
+			})
+
+			It("can no longer handle requests for the stream", func() {
+				Eventually(fakeHandler.StopCalled).Should(BeTrue())
+				_, err := manager.SendRequest(1234, nil)
+				Expect(err).To(MatchError("stream provider 1234 not found"))
+			})
+		})
+	})
+})
