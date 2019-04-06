4704c3cab209bdeee14d781d15b76c3edb581d68
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-04-05 23:27:12 -0700

    Fix ordering problems when testing Collection.Populate

--- backend/datasheet/collection.go
+++ backend/datasheet/collection.go
@@ -15,32 +15,37 @@ type Collection struct {
 	RecipeData   RecipeStore
 }
 
+type dataTuple struct {
+	path      string
+	populator func(io.Reader) error
+}
+
 func (c *Collection) Populate(dataPath string) error {
-	dataMapping := map[string]func(io.Reader) error{
-		filepath.Join(dataPath, "Map.csv"):           c.MapData.PopulateMaps,
-		filepath.Join(dataPath, "TerritoryType.csv"): c.MapData.PopulateTerritories,
-		filepath.Join(dataPath, "PlaceName.csv"):     c.MapData.PopulatePlaceNames,
+	dataMapping := []dataTuple{
+		{filepath.Join(dataPath, "Map.csv"), c.MapData.PopulateMaps},
+		{filepath.Join(dataPath, "TerritoryType.csv"), c.MapData.PopulateTerritories},
+		{filepath.Join(dataPath, "PlaceName.csv"), c.MapData.PopulatePlaceNames},
 
-		filepath.Join(dataPath, "BNpcName.csv"):      c.BNPCData.PopulateBNPCNames,
-		filepath.Join(dataPath, "BNpcBase.csv"):      c.BNPCData.PopulateBNPCBases,
-		filepath.Join(dataPath, "ModelChara.csv"):    c.BNPCData.PopulateModelCharas,
-		filepath.Join(dataPath, "ModelSkeleton.csv"): c.BNPCData.PopulateModelSkeletons,
+		{filepath.Join(dataPath, "BNpcName.csv"), c.BNPCData.PopulateBNPCNames},
+		{filepath.Join(dataPath, "BNpcBase.csv"), c.BNPCData.PopulateBNPCBases},
+		{filepath.Join(dataPath, "ModelChara.csv"), c.BNPCData.PopulateModelCharas},
+		{filepath.Join(dataPath, "ModelSkeleton.csv"), c.BNPCData.PopulateModelSkeletons},
 
-		filepath.Join(dataPath, "Action.csv"):      c.ActionData.PopulateActions,
-		filepath.Join(dataPath, "Omen.csv"):        c.ActionData.PopulateOmens,
-		filepath.Join(dataPath, "CraftAction.csv"): c.ActionData.PopulateCraftActions,
+		{filepath.Join(dataPath, "Action.csv"), c.ActionData.PopulateActions},
+		{filepath.Join(dataPath, "Omen.csv"), c.ActionData.PopulateOmens},
+		{filepath.Join(dataPath, "CraftAction.csv"), c.ActionData.PopulateCraftActions},
 
-		filepath.Join(dataPath, "Status.csv"): c.StatusData.PopulateStatuses,
+		{filepath.Join(dataPath, "Status.csv"), c.StatusData.PopulateStatuses},
 
-		filepath.Join(dataPath, "ClassJob.csv"): c.ClassJobData.PopulateClassJobs,
+		{filepath.Join(dataPath, "ClassJob.csv"), c.ClassJobData.PopulateClassJobs},
 
-		filepath.Join(dataPath, "Recipe.csv"):           c.RecipeData.PopulateRecipes,
-		filepath.Join(dataPath, "RecipeLevelTable.csv"): c.RecipeData.PopulateRecipeLevelTable,
-		filepath.Join(dataPath, "Item.csv"):             c.RecipeData.PopulateItems,
+		{filepath.Join(dataPath, "Recipe.csv"), c.RecipeData.PopulateRecipes},
+		{filepath.Join(dataPath, "RecipeLevelTable.csv"), c.RecipeData.PopulateRecipeLevelTable},
+		{filepath.Join(dataPath, "Item.csv"), c.RecipeData.PopulateItems},
 	}
 	fileReader := new(FileReader)
-	for dataPath, populator := range dataMapping {
-		fileReader.ReadFile(dataPath, populator)
+	for _, t := range dataMapping {
+		fileReader.ReadFile(t.path, t.populator)
 	}
 	return fileReader.Error()
 }
