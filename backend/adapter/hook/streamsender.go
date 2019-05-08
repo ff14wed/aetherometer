508bbba10ed6701568fffb70612a757460732266
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-05-08 01:55:15 -0700

    Add hook stream sender

---
+++ backend/adapter/hook/streamsender.go
@@ -0,0 +1,68 @@
+package hook
+
+import (
+	"errors"
+	"io"
+
+	"go.uber.org/zap"
+)
+
+//go:generate counterfeiter io.WriteCloser
+
+// StreamSender listens for requests and sends data to the hook.
+type StreamSender struct {
+	hookConn io.WriteCloser
+	logger   *zap.Logger
+
+	sendChan chan Envelope
+	stopDone chan struct{}
+}
+
+// NewStreamSender creates a new StreamSender
+func NewStreamSender(hookConn io.WriteCloser, logger *zap.Logger) *StreamSender {
+	return &StreamSender{
+		hookConn: hookConn,
+		logger:   logger,
+
+		sendChan: make(chan Envelope),
+		stopDone: make(chan struct{}),
+	}
+}
+
+// Serve runs the service responsible for handling requests to send data to the
+// hook connection.
+func (s *StreamSender) Serve() {
+	defer close(s.stopDone)
+
+	logger := s.logger.Named("stream-sender")
+	logger.Info("Running")
+
+	for {
+		envelope, ok := <-s.sendChan
+		if !ok {
+			logger.Info("Stopping...")
+			return
+		}
+		envBytes := envelope.Encode()
+		n, err := s.hookConn.Write(envBytes)
+		if err != nil {
+			logger.Error("writing bytes to conn", zap.Error(err))
+		}
+		if n < len(envBytes) {
+			err := errors.New("wrote less than the envelope length")
+			logger.Error("writing bytes to conn", zap.Error(err))
+		}
+	}
+}
+
+// Stop will shutdown this service and wait on it to stop before returning.
+func (s *StreamSender) Stop() {
+	close(s.sendChan)
+	<-s.stopDone
+}
+
+// Send queues a request to send the data as an Envelope through the hook
+// connection.
+func (s *StreamSender) Send(op byte, data uint32, additional []byte) {
+	s.sendChan <- Envelope{Op: op, Data: data, Additional: additional}
+}
---
+++ backend/adapter/hook/streamsender_test.go
@@ -0,0 +1,116 @@
+package hook_test
+
+import (
+	"errors"
+	"net/url"
+	"sync"
+
+	"github.com/ff14wed/sibyl/backend/adapter/hook"
+	"github.com/ff14wed/sibyl/backend/adapter/hook/hookfakes"
+	"github.com/ff14wed/sibyl/backend/testhelpers"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/thejerf/suture"
+	"go.uber.org/zap"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("StreamSender", func() {
+	var (
+		ss *hook.StreamSender
+
+		hookConn *hookfakes.FakeWriteCloser
+
+		logBuf *testhelpers.LogBuffer
+		once   sync.Once
+
+		supervisor *suture.Supervisor
+	)
+
+	BeforeEach(func() {
+		once.Do(func() {
+			logBuf = new(testhelpers.LogBuffer)
+			err := zap.RegisterSink("streamsendertest", func(*url.URL) (zap.Sink, error) {
+				return logBuf, nil
+			})
+			Expect(err).ToNot(HaveOccurred())
+		})
+		logBuf.Reset()
+		zapCfg := zap.NewDevelopmentConfig()
+		zapCfg.OutputPaths = []string{"streamsendertest://"}
+		logger, err := zapCfg.Build()
+		Expect(err).ToNot(HaveOccurred())
+
+		hookConn = new(hookfakes.FakeWriteCloser)
+		ss = hook.NewStreamSender(hookConn, logger)
+
+		supervisor = suture.New("test-provider", suture.Spec{
+			Log: func(line string) {
+				_, _ = GinkgoWriter.Write([]byte(line))
+			},
+			FailureThreshold: 1,
+		})
+		supervisor.ServeBackground()
+		_ = supervisor.Add(ss)
+
+		hookConn.WriteStub = func(d []byte) (int, error) {
+			return len(d), nil
+		}
+	})
+
+	AfterEach(func() {
+		supervisor.Stop()
+	})
+
+	It(`logs "Running" on startup`, func() {
+		Eventually(logBuf).Should(gbytes.Say("stream-sender.*Running"))
+	})
+
+	It(`logs "Stopping..." on shutdown`, func() {
+		supervisor.Stop()
+		Eventually(logBuf).Should(gbytes.Say("stream-sender.*Stopping..."))
+	})
+
+	It("encodes and sends envelopes along the hook connection", func() {
+		ss.Send(200, 1234, []byte("Hello World"))
+		Eventually(hookConn.WriteCallCount).Should(Equal(1))
+		Expect(hookConn.WriteArgsForCall(0)).To(Equal(append([]byte{
+			20, 0, 0, 0, // Length
+			200,          // Op
+			210, 4, 0, 0, // Data
+		}, []byte("Hello World")...)))
+	})
+
+	Context("when the writer wrote fewer bytes than expected", func() {
+		BeforeEach(func() {
+			hookConn.WriteStub = nil
+			hookConn.WriteReturns(0, nil)
+		})
+
+		It("logs an error, but continues running", func() {
+			ss.Send(200, 1234, []byte("Hello World"))
+			Eventually(hookConn.WriteCallCount).Should(Equal(1))
+			Eventually(logBuf).Should(gbytes.Say("ERROR.*stream-sender.*writing bytes to conn.*wrote less than the envelope length"))
+
+			ss.Send(200, 1234, []byte("Hello World"))
+			Eventually(hookConn.WriteCallCount).Should(Equal(2))
+		})
+	})
+
+	Context("when the writer returns an error writing", func() {
+		BeforeEach(func() {
+			hookConn.WriteStub = nil
+			hookConn.WriteReturns(0, errors.New("Boom"))
+		})
+
+		It("logs an error, but continues running", func() {
+			ss.Send(200, 1234, []byte("Hello World"))
+			Eventually(hookConn.WriteCallCount).Should(Equal(1))
+			Eventually(logBuf).Should(gbytes.Say("ERROR.*stream-sender.*writing bytes to conn.*Boom"))
+
+			ss.Send(200, 1234, []byte("Hello World"))
+			Eventually(hookConn.WriteCallCount).Should(Equal(2))
+		})
+	})
+})
